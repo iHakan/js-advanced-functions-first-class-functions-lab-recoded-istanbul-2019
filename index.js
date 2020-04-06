e8b7aa251415b864267aac9cfd98bc26e4c5c25a
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 22:53:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(int){
+  return function(){
+    return int**2;
+  }
+}
